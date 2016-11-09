4b0cf5e5bdc4baae9421bd87c8a7c6b458ddf21b
Priyanka Chaudhary <priyanka.chaudhary18@gmail.com>
2016-11-09 15:41:07 +0000

    main.m edited by Priyanka

--- Exercise_2/main.m
+++ Exercise_2/main.m
@@ -22,3 +22,27 @@ hold on;
 for i=1:size(points,1)
    plot(points(i,2),points(i,1),'yo','MarkerSize',points(i,3)^2); 
 end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Harris
+
+n = 0;
+s0 = 1.5;
+k = 1.2;
+alpha = 0.04;
+t = 0.1;
+
+img1 = 'test.pgm';
+img2= 'house.tif';
+img3 = 'sample2.jpg';
+img4 = 'checkerboard_tunnel.png';
+
+% I=double(rgb2gray(imread(img3)))/255.0;
+I=double(imread(img3))/255.0;
+pt=harris_det(I,n, s0, k, alpha, t);
+[x, y] = ind2sub(size(pt), find(pt>0));
+imagesc(I), colormap gray, axis equal tight off;
+% imshow(I);
+hold on;
+plot(x,y,'r.','MarkerSize',10)
