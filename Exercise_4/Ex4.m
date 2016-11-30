31d027702a61c0e3919450dd9f88b9999509846d
Priyanka Chaudhary <priyanka.chaudhary18@gmail.com>
2016-11-30 21:28:41 +0000

    Ex4.m edited added tree generation part

--- Exercise_4/Ex4.m
+++ Exercise_4/Ex4.m
@@ -1,5 +1,55 @@
 close all;
 clear;
+
+numberOfTree = 10;
+trees = cell(numberOfTree, 2);
+
+for n = 0:numberOfTree-1
+    
+    %reading tree files
+    filename = sprintf('Tree%d.txt', n);
+    file = fopen(filename);
+    
+    %storing number of nodes
+    N = textscan(file, '%d', 1);
+    cell_node = textscan(file, '%d %d %d %f %d %d %d %d %d %d %d', N{1});
+    
+    %storing the attributes of nodes from cells into array
+    nodes = zeros(N{1}, numel(cell_node));
+    for i = 1:numel(cell_node)
+        nodes(:, i) = cell_node{i};
+    end    
+    
+    %changing color columns as mentioned in FAQ from BGR to RGB
+    nodes(:, 7) = colorChannelAssignment(nodes(:, 7));
+    nodes(:, 10) = colorChannelAssignment(nodes(:, 10));
+    
+    %storing number of leafs
+    M = textscan(file, '%d', 1);
+    cell_leaf = textscan(file, '%d %f %f', M{1});
+    
+    %storing the attributes of leafs from cells into array
+    leafs = zeros(M{1}, numel(cell_leaf));
+    for i = 1:numel(cell_leaf)
+        leafs(:, i) = cell_leaf{i};
+    end
+    
+    %adding tree attributes to cell
+    %1st column cells is for nodes
+    %2nd column cells for leafs
+    trees{n+1,1} = nodes;
+    trees{n+1,2} = leafs;
+end
+
+function rgb = colorChannelAssignment(rgb)
+if (rgb == 0)
+    rgb = 2;
+end
+if (rgb == 0)
+    rgb = 0;
+end
+end
+
 %% Get the Integral Image
 I = double(imread('Ex04Files/2007_000032.jpg'));
 Y_s = size(img, 1);
