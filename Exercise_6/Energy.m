888a775f7c9e6e49b85775f1f93517daff320c59
Azade <azadefarshad@gmail.com>
2016-12-14 21:21:45 +0100

    energy fixed

--- Exercise_6/Energy.m
+++ Exercise_6/Energy.m
@@ -1,7 +1,8 @@
 function E = Energy(R,T,A,M,m)
     %Generating the euler angles from tip3, not sure
     R = rotm2eul(R);
-    d = A*(R*M+T);
+    %d = A*(M*R'+T);
+    d = (M*R'+repmat(T', [size(M, 1), 1]))*A';
     %normalizing the 2D projection by dividing by z
     d = d(:, 1:2) ./ repmat(d(:, 3), [1 2]); 
     E =sum((d-m).^2);
