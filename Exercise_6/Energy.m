515c10a0faed5f275e03876838a475cd958e0db2
Priyanka Chaudhary <priyanka.chaudhary18@gmail.com>
2016-12-15 00:07:06 +0000

    new

--- Exercise_6/Energy.m
+++ Exercise_6/Energy.m
@@ -1,9 +1,24 @@
 function E = Energy(R,T,A,M,m)
     %Generating the euler angles from tip3, not sure
-    R = rotm2eul(R);
-    %d = A*(M*R'+T);
-    d = (M*R'+repmat(T', [size(M, 1), 1]))*A';
-    %normalizing the 2D projection by dividing by z
-    %d = d(:, 1:2) ./ repmat(d(:, 3), [1 2]); 
-    E =sum(sum((d-m).^2));
+    
+    ra = R(1);
+    rb = R(2);
+    rg = R(3);
+    t1 = T(1);
+    t2 = T(2);
+    t3 = T(3);
+
+    R = rotation_matrix(R);
+    T = [t1;t2;t3];
+    RT = [R,T];
+    
+    E = sum(sum((A*RT*M-m).^2));
+    
+%     R = rotm2eul(R);
+%     %d = A*(M*R'+T);
+%     RT = horzcat(R,T);
+%     d = (M*R'+repmat(T', [size(M, 1), 1]))*A';
+%     %normalizing the 2D projection by dividing by z
+%     %d = d(:, 1:2) ./ repmat(d(:, 3), [1 2]); 
+%     E =sum(sum((d-m).^2));
 end
