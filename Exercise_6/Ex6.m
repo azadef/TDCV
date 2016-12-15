89e7ddff1ddd59fa20d5d0a7e9f87004ab01c9db
Azade Farshad <azadefarshad@gmail.com>
2016-12-15 09:47:29 +0000

    Ex6.m edited

--- Exercise_6/Ex6.m
+++ Exercise_6/Ex6.m
@@ -5,6 +5,7 @@ close all;
 clc;
 
 %run('D:\MS Informatics\3rd Semester\Tracking and Detection\Exercises\3rd Exercise/VLFEATROOT/toolbox/vl_setup');
+run('vlfeat-0.9.20/toolbox/vl_setup');
 
 Ia_2 = single(rgb2gray(imread('img_sequence/0000.png')))/255;
 Ib_2 = single(rgb2gray(imread('img_sequence/0000.png')))/255;
@@ -39,13 +40,26 @@ mScene = fb(1:2,matches(2,:))' ;
 showMatchedFeatures(Ia_2,Ia_2, fa(1:2,matches(1,:))' ,fb(1:2,matches(2,:))','montage');
 title('all matched features')
 
+corners = [103,75;
+           550,75;
+           105,385;
+           553,385];
 
-m = padarray(matches, [1 0], 1, 'post');
+min_x = min(corners(:,1));
+max_x = max(corners(:,1));
+min_y = min(corners(:,2));
+max_y = max(corners(:,2));
+valid = fa(1,:) > min_x & fa(1,:) <= max_x & ...
+             fa(2,:) > min_y & fa(2,:) <= max_y;
+fa_2 = fa(:, valid);
+da_2 = da(:, valid);
+m = [fa_2(1:2, :); ones(1,size(fa_2, 2))];
+%m = padarray(matches, [1 0], 1, 'post');
 A = [472.3,0.64,329.0; 0,471.0,268.3; 0,0,1];
 Ainv = inv(A);
 Mi0 = A \ m;
-Mi0 = Mi0';
-
+%Mi0 = Mi0';
+Mi0 = [Mi0; ones(1,size(fa_2, 2))];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -65,15 +79,20 @@ mT = cell(n,1);
 for i=1:n
     [fb, db] = vl_sift(images{i}) ;
     % This should be replaced by the points found in part1 
-    [matches, scores] = vl_ubcmatch(da, db) ;
-    m0{i} = fa(1:2,matches(1,:))' ;
+    [matches, scores] = vl_ubcmatch(da_2, db) ;
+    m0{i} = fa_2(1:2,matches(1,:))' ;
     mT{i} = fb(1:2,matches(2,:))' ;
-    [H, m0{i}, mT{i}] = ransac(m0{i}, mT{i}, 80, 25, 20000);
+    %[H, m0{i}, mT{i}] = ransac(m0{i}, mT{i}, 80, 25, 20000);
+    [H, m0{i}, mT{i}] = estimateGeometricTransform(...
+        m0{i}, ...
+        mT{i}, 'affine', ...
+        'MaxNumTrials', 4000, ...
+        'MaxDistance', 4);
     out_result{i} = showMatchedFeatures(images{1}, images{i}, m0{i}, mT{i}, 'montage');
     saveas(gcf,[out_dir img_dir(i).name]);
 end
 
-Rt = zeros(1,3,nr_images);
+Rt = zeros(1,3,n);
 % initial rotation is all zeros
 Rt(:,:,1) = [0, 0, 0];    
 
