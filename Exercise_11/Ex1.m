4ef8503e5cdd539ffc6058f64f6acbd2dbecffe7
Azade <azadefarshad@gmail.com>
2017-02-08 16:28:21 +0100

    fixed ssd

--- Exercise_11/Ex1.m
+++ Exercise_11/Ex1.m
@@ -3,11 +3,11 @@ clear;
 close all;
 
 %% Part 1
-scales = 2 .^ (4:-1:0);
+scales = 2 .^ (5:-1:0);
 %scales = 1;
-%I = double(imread('lena.gif'));
+%I = imread('lena.gif');
 I = imread('monalisa.jpg');
-I = rgb2gray(I);
+%I = rgb2gray(I);
 
 figure;
 imagesc(I); hold on; colormap gray;
@@ -15,7 +15,7 @@ imagesc(I); hold on; colormap gray;
 I = double(I);
 rect = round(getrect());
 T = I(rect(2):rect(2)+rect(4),rect(1):rect(1)+rect(3),:);
-disp([rect(1),rect(2)]);
+disp(['First pos: ' num2str(rect(1)) ',' num2str(rect(2))]);
 %I_sampled = I(1:2:size(I_sampled,1),1:2:size(I_sampled,2));
 %T_sampled = T;
 
@@ -24,7 +24,10 @@ ssd_ystart = 1;
 ncc_xstart = 1;
 ncc_ystart = 1;
 for i = scales
-    disp(i);
+    disp(['Scale: ' num2str(i)]);
+    
+    %ssd_score = zeros(round(size(I,1)/i)-round(size(T,1)/i)-ssd_xstart, ...
+    %    round(size(I,2)/i)-round(size(T,2)/i)-ssd_ystart,size(I,3));
     ssd_score = zeros(size(I));
     ncc_score = zeros(size(I));
     for z=1:size(I,3)
@@ -45,8 +48,12 @@ for i = scales
         end
     end
     ssd_score = mean(ssd_score,3);
+%     cols = sum(ssd_score,1);
+%     rows = sum(ssd_score,2);
+%     ssd_score(:,~cols) = [];
+%     ssd_score(~rows,:) = [];
     ncc_score = mean(ncc_score,3);
-    ssdmax=find(ssd_score(:)==max(ssd_score(:)));
+    ssdmax=find(ssd_score(:)==min(nonzeros(ssd_score(:))));
     nccmax=find(ncc_score(:)==max(ncc_score(:)));
     [ssd_xmax,ssd_ymax]=ind2sub(size(ssd_score),ssdmax);
     [ncc_xmax,ncc_ymax]=ind2sub(size(ncc_score),nccmax);
@@ -55,12 +62,12 @@ for i = scales
     ssd_ystart = min(ssd_ymax);
     ncc_xstart = min(ncc_xmax);
     ncc_ystart = min(ncc_ymax);
-    disp([ssd_xstart,ssd_ystart]);
-    disp([ncc_xstart,ncc_ystart]);
+    disp(['SSD: ' num2str(ssd_xstart) ' , ' num2str(ssd_ystart)]);
+    disp(['NCC: ' num2str(ncc_xstart) ' , ' num2str(ncc_ystart)]);
     
 end
 
-ssd_rect = [ssd_xstart,ssd_ystart, rect(3),rect(4)];
-ncc_rect = [ncc_xstart,ncc_xstart, rect(3),rect(4)];
+ssd_rect = [ssd_xstart+1,ssd_ystart+1, rect(3),rect(4)];
+ncc_rect = [ncc_xstart+2,ncc_ystart+2, rect(3),rect(4)];
 rectangle('Pos', ssd_rect,'EdgeColor','g');
 rectangle('Pos', ncc_rect,'EdgeColor','r');
--- Exercise_11/SSD.m
+++ Exercise_11/SSD.m
@@ -1,4 +1,4 @@
 function score =SSD(I,T,x,y)
     [m,n,~] = size(T);
-    score = sum(sum((T-I(x:x+m-1,y:y+n-1)).^2));
+    score = sum(sum((T-I(x:x+m-1,y:y+n-1)).^2)) + 1;
 end
